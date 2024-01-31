6a4a545136a1bc1b99909151a43ed2c3f4de4c13
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-01-31 21:55:32 +0000

    Learning how to read and write files.

---
+++ file_handling.py
@@ -0,0 +1,3 @@
+file_object = open(r"File_name", "w")
+with open('File_name', 'a') as file:
+    file.write('Eldridge is king')
--- shift_tracker.py
+++ shift_tracker.py
@@ -4,38 +4,44 @@ import re
 acc_holders = {}
 
 
-def create_account():
-    email = input("Enter your email: ")
-    if email in acc_holders:
-        print("You already have an account. Try logging in")
-    else:
-        password = input("Create a new password: ")
-    acc_holders[email] = password
-    with open('user_credentials.txt', 'w') as file:
-        file.write(f"Email: {email}\n")
-        file.write(f"Password: {password}\n")
-
-
-def login():
-    email = input("Enter your email address: ")
-    if email in acc_holders:
-        password = input("Enter your password: ")
-        if acc_holders[email] == password:
-            print("Welcome to your acount")
-        else:
-            input("You have entered a wrong password. Please try again!: ")
+# def create_account():
+#     email = input("Enter your email: ")
+#     if email in acc_holders:
+#         print("You already have an account. Try logging in")
+#     else:
+#         password = input("Create a new password: ")
+#     acc_holders[email] = password
+#     with open('user_credentials.txt', 'a') as file:
+#         file.write(email | password)
+#
+#
+#
+# def login():
+#     email = input("Enter your email address: ")
+#     if email in acc_holders:
+#         password = input("Enter your password: ")
+#         if acc_holders[email] == password:
+#             print("Welcome to your acount")
+#         else:
+#             input("You have entered a wrong password. Please try again!: ")
+#     else:
+#         print("User does not exist. Create an account.")
 
 def main():
     print("Welcome to your Shift tracker.")
-    choice = input("If you want to create an account, press 'E'."
-                   "If you want to login, Press 'E'."
-                   "Else, press 'q' to Quit: ")
-    if choice.lower() == 'e':
-        create_account()
-    elif choice.lower() == "l":
-        login()
-    else:
-        quit()
+    # choice = input("If you want to create an account, press 'E'."
+    #                "If you want to login, Press 'E'."
+    #                "Else, press 'q' to Quit: ")
+
+    email = input("Please enter your email: ")
+    with open('user_credentials.txt', 'r') as file:
+        credentials = file.readlines()
+
+    for cred in credentials:
+        if email in any(cred):
+            print('This works')
+        else:
+            print('Fuck off')
 
 
 

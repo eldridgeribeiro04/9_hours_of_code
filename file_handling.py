b6757972a129c42e5d8c43d3a87b2da5aea1d635
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-02-01 21:37:55 +0000

    New features

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,4 @@
 file_object = open(r"File_name", "w")
 with open('File_name', 'a') as file:
-    file.write('Eldridge is king')
+    file.write('Also a nice person\n')
+file.close()
--- shift_tracker.py
+++ shift_tracker.py
@@ -1,48 +1,54 @@
-"""Create a 'create account' page"""
 import re
 
-acc_holders = {}
+
+def create_account(email_add):
+    email = email_add
+    password = input("Create a new password: ")
+    with open('user_credentials.txt', 'a') as file:
+        file.write(email + '|' + password + '\n')
+    option = input('Do you want to login? (Press Y for yes or q to quit: ')
+    if option.lower() == 'y':
+        login()
+    else:
+        quit()
 
 
-# def create_account():
-#     email = input("Enter your email: ")
-#     if email in acc_holders:
-#         print("You already have an account. Try logging in")
-#     else:
-#         password = input("Create a new password: ")
-#     acc_holders[email] = password
-#     with open('user_credentials.txt', 'a') as file:
-#         file.write(email | password)
-#
-#
-#
-# def login():
-#     email = input("Enter your email address: ")
-#     if email in acc_holders:
-#         password = input("Enter your password: ")
-#         if acc_holders[email] == password:
-#             print("Welcome to your acount")
-#         else:
-#             input("You have entered a wrong password. Please try again!: ")
-#     else:
-#         print("User does not exist. Create an account.")
+def check_credentials(user_email, user_password, filename='user_credentials.txt'):
+    with open('user_credentials.txt', 'r') as file:
+        for line in file:
+            stored_email, stored_password = line.strip().split('|')
+            if user_email == stored_email and user_password == stored_password:
+                return True
+    return False
+
+
+def login():
+    email = input("Enter your email address: ")
+    password = input('Enter your password: ')
+    if check_credentials(email, password):
+        print('Welcome to your account')
+    else:
+        option = input('Incorrect Email or password. Press enter to try again(Press q to quit:')
+        if option.lower() != 'q':
+            login()
+        else:
+            quit()
+
 
 def main():
     print("Welcome to your Shift tracker.")
-    # choice = input("If you want to create an account, press 'E'."
-    #                "If you want to login, Press 'E'."
-    #                "Else, press 'q' to Quit: ")
 
-    email = input("Please enter your email: ")
+    email_add = input("Please enter your email: ")
     with open('user_credentials.txt', 'r') as file:
         credentials = file.readlines()
 
     for cred in credentials:
-        if email in any(cred):
-            print('This works')
+        if email_add in cred:
+            print('This email already exists, please try a different email or use the login page!')
+            option = input('Do you want to login?(Y for yes, N for no): ')
+            if option.lower() == 'y':
+                login()
         else:
-            print('Fuck off')
-
-
+            create_account(email_add)
 
 main()
